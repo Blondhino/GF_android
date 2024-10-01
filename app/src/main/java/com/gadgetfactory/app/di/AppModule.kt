34f1a52eded9801a3b6dc3b38c40d54536f27210
Enio <ebtronic15@gmail.com>
2024-10-01 14:26:41 +0200

    feat: initialize koin and DI graph

---
+++ app/src/main/java/com/gadgetfactory/app/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.gadgetfactory.app.di
+
+import com.gadgetfactory.app.home.di.homeModule
+import com.gadgetfactory.app.splash.di.splashModule
+
+
+val appModule = listOf(
+    homeModule,
+    splashModule,
+)
