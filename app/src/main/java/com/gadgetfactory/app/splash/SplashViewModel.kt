9ac55cc3065b6dae30f160a446e283c20fa34e72
Enio <ebtronic15@gmail.com>
2024-10-13 18:36:27 +0200

    chore: make SplashScreen as default destination when entering app and make it decide next destination (Auth/GadgetCenter)

--- app/src/main/java/com/gadgetfactory/app/splash/SplashViewModel.kt
+++ app/src/main/java/com/gadgetfactory/app/splash/SplashViewModel.kt
@@ -1,10 +1,37 @@
 package com.gadgetfactory.app.splash
 
+import arrow.core.Either
 import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import com.gadgetfactory.app.auth.domain.usecase.CurrentUser
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class SplashViewModel : ScreenModel {
+class SplashViewModel(
+    private val currentUser: CurrentUser,
+) : ScreenModel {
+    private val _startDestination = MutableStateFlow<StartDestination?>(null)
+    val startDestination = _startDestination
+        .onStart { calculateInitialDestination() }
+        .stateIn(
+            scope = screenModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = null,
+        )
 
-    fun getText() = TEXT
+    private fun calculateInitialDestination() = screenModelScope.launch {
+        when (currentUser()) {
+            is Either.Left -> _startDestination.update { StartDestination.Auth }
+            is Either.Right -> _startDestination.update { StartDestination.GadgetCenter }
+        }
+    }
 }
 
-private const val TEXT = "Splash View Model"
+sealed interface StartDestination {
+    data object Auth : StartDestination
+    data object GadgetCenter : StartDestination
+}
