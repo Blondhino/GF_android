6cc95c194f6b90e7b93f95891809ee95622104c1
eniobiondic <enio.biondic@undabot.com>
2025-04-30 13:50:00 +0200

    fix: add missing logging lib required by ktor, and update proguard-rules.pro file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,10 @@ android {
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += setOf(
+                "/META-INF/{AL2.0,LGPL2.1}",
+                "META-INF/INDEX.LIST",
+            )
         }
     }
 }
@@ -77,6 +80,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.logback.classic)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
