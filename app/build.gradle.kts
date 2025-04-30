f3875960fde96cd6c8f6aa0d3a7291105b40f007
eniobiondic <enio.biondic@undabot.com>
2025-04-30 14:25:22 +0200

    fix: replaced logging lib logbackClassic -> slf4jNop

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,9 +78,9 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.slf4j.nop)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.logback.classic)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
