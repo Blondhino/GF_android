88c601057b60008360883a1db38a2ca6958d1769
eniobiondic <enio.biondic@undabot.com>
2025-05-13 13:49:05 +0200

    chore: removed unused home package

--- app/src/main/java/com/gadgetfactory/app/core/di/AppModule.kt
+++ app/src/main/java/com/gadgetfactory/app/core/di/AppModule.kt
@@ -3,12 +3,10 @@ package com.gadgetfactory.app.core.di
 import com.gadgetfactory.app.auth.di.authModule
 import com.gadgetfactory.app.core.coreModule
 import com.gadgetfactory.app.gadgetcenter.di.gadgetCenterModule
-import com.gadgetfactory.app.home.di.homeModule
 import com.gadgetfactory.app.scan.di.registerDeviceModule
 import com.gadgetfactory.app.splash.di.splashModule
 
 val appModule = listOf(
-    homeModule,
     splashModule,
     coreModule,
     authModule,
